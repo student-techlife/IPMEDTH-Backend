b22ac8062e2c529dab9bae57de534293de9d02cf
Klaas Schoute <klaas_schoute@hotmail.com>
2022-05-17 22:38:46 +0200

    Basic migration/model setup

---
+++ laravel/app/Models/Measurement.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Measurement extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'patient_id',
+        'user_id',
+    ];
+
+    /**
+     * Relationships
+     */
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ laravel/app/Models/Patient.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'date_of_birth',
+        'email',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'date_of_birth' => 'datetime',
+    ];
+
+    /**
+     * Relationships
+     */
+    public function measurements()
+    {
+        return $this->hasMany(Measurement::class);
+    }
+}
