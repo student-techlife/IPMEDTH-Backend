6fe67f41be1cb997b90481d11109d30c465953cc
Klaas Schoute <klaas_schoute@hotmail.com>
2022-06-10 14:33:44 +0200

    Add flowbite as extra frontend styling framework

--- laravel/webpack.mix.js
+++ laravel/webpack.mix.js
@@ -1,4 +1,10 @@
 const mix = require('laravel-mix');
+const lodash = require("lodash");
+const folder = {
+    src: "resources/", // source files
+    dist: "public/", // build files
+    dist_assets: "public/assets/" //build assets files
+};
 
 /*
  |--------------------------------------------------------------------------
@@ -10,6 +16,40 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+
+var third_party_assets = {
+    css_js: [
+        {"name": "flowbite", "assets": ["./node_modules/flowbite/dist/flowbite.js", "./node_modules/flowbite/dist/flowbite.min.css"]},
+    ]
+}
+
+//copying third party assets
+lodash(third_party_assets).forEach(function (assets, type) {
+    if (type == "css_js") {
+        lodash(assets).forEach(function (plugin) {
+            var name = plugin['name'],
+                assetlist = plugin['assets'],
+                css = [],
+                js = [];
+            lodash(assetlist).forEach(function (asset) {
+                var ass = asset.split(',');
+                for (let i = 0; i < ass.length; ++i) {
+                    if(ass[i].substr(ass[i].length - 3)  == ".js") {
+                        js.push(ass[i]);
+                    } else {
+                        css.push(ass[i]);
+                    }
+                };
+            });
+            if(js.length > 0){
+                mix.combine(js, folder.dist_assets + "/libs/" + name + "/" + name + ".min.js");
+            }
+            if(css.length > 0){
+                mix.combine(css, folder.dist_assets + "/libs/" + name + "/" + name + ".min.css");
+            }
+        });
+    }
+});
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
