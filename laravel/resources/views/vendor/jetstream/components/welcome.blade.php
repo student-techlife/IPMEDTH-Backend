d26c62400d1a03fcf8b883a32c8d0f4db0396986
Klaas Schoute <klaas_schoute@hotmail.com>
2022-06-11 02:14:02 +0200

    Add default jetstream blade files

---
+++ laravel/resources/views/vendor/jetstream/components/welcome.blade.php
@@ -0,0 +1,48 @@
+<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+    <div>
+        <x-jet-application-logo class="block h-12 w-auto" />
+    </div>
+
+    <div class="mt-8 text-2xl">
+        Welkom op beheerpanel van het handbogen project!
+    </div>
+
+    <div class="mt-6 text-gray-500">
+        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
+        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
+        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
+        ecosystem to be a breath of fresh air. We hope you love it.
+    </div>
+</div>
+
+<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
+    <div class="p-6">
+        <div class="flex items-center">
+            <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-gray-400"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://laravel.com/docs">Patienten</a></div>
+        </div>
+    </div>
+
+    <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
+        <div class="flex items-center">
+            <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-gray-400"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://laracasts.com">Resultaten</a></div>
+        </div>
+    </div>
+
+    <div class="p-6 border-t border-gray-200">
+        <div class="flex items-center">
+            <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-gray-400"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://tailwindcss.com/">Sessies</a></div>
+        </div>
+    </div>
+
+    <a href="/api/docs" class="group" target="_blank">
+        <div class="p-6 border-t border-gray-200 hover:bg-rf-orange md:border-l">
+            <div class="flex items-center">
+                <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-gray-400 group-hover:text-white"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
+                <div class="ml-4 text-lg text-gray-600 group-hover:text-white leading-7 font-semibold">API Documentatie</div>
+            </div>
+        </div>
+    </a>
+</div>
