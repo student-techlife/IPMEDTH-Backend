5ea3d8dd83d1dabc04c37f6a9c86efd2005d6d54
Klaas Schoute <klaas_schoute@hotmail.com>
2022-06-13 20:35:09 +0200

    Set permissions on navigation routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\RoleController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 // Get images from storage
